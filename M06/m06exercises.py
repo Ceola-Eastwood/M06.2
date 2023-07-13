6de42afd781c0fe679b90eb1f282c6c3eb15dff4
Ceola-Eastwood <135837902+Ceola-Eastwood@users.noreply.github.com>
2023-07-13 12:59:17 -0400

    Add files via upload

---
+++ M06/m06exercises.py
@@ -0,0 +1,24 @@
+#
+#
+import multiprocessing
+from datetime import date
+
+#now = date.today()
+#now_str = now.isoformat()
+#with open('today.txt', 'wt') as output:
+    #print(now_str, file=output)
+
+
+
+def now(seconds):
+    from datetime import datetime
+    from time import sleep
+    sleep(seconds)
+    print('wait', seconds, 'seconds, time is', datetime.utcnow())
+
+if __name__ == '__main__':
+    import random
+    for n in range(3):
+        seconds = random.random()
+        proc = multiprocessing.Process(target=now, args=(seconds,))
+        proc.start()
